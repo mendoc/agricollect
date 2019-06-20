f5190c775113c4381c3113ba1232cda90296ce0a
Din <din.kelby7@gmail.com>
2019-06-20 11:57:56 +0200

    nouvelle interface pour la reinitialisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-// import 'package:agricollect/route/splashscreen.dart';
-import 'package:agricollect/route/reset_page.dart';
-=======
 import 'package:agricollect/config.dart';
-import 'package:agricollect/route/splashscreen.dart';
->>>>>>> bcbe9f880a2b0d1f67751e44074118d9f748dc19
+import 'package:agricollect/route/reset_page.dart';
+// import 'package:agricollect/route/inscription.dart';
+// import 'package:agricollect/route/reset_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
