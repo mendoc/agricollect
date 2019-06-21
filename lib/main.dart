ff54f0d1168441ab0a1137322e38a7d07347d140
Din <din.kelby7@gmail.com>
2019-06-21 12:54:34 +0200

    Duplicata de new campaign pour public campaign

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:agricollect/config.dart';
-import 'package:agricollect/route/personal_campaign.dart';
+import 'package:agricollect/route/public_campaign.dart';
 // import 'package:agricollect/route/inscription.dart';
 // import 'package:agricollect/route/reset_page.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: mainColor),
       title: 'AGRI COLLECT',
-      home: PersonalCampaign(),
+      home: PublicCampaign(),
     );
   }
 }
---
+++ lib/route/public_campaign.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+class PublicCampaign extends StatefulWidget {
+  @override
+  _PublicCampaignState createState() => _PublicCampaignState();
+}
+
+class _PublicCampaignState extends State<PublicCampaign> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: 10, left: 25, right: 25),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: new BorderRadius.circular(15.0),
+                child: Image.asset('assets/img/7.jpg'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 1.0, left: 5, right: 5, bottom: 8),
+                    child: Text(
+                      'Cette campagne a étét menée dans le but de connaitre la quantité de citron produite dans la zone de Makokou. Il ressort de la que les citrons sont beaucoup plus produit dans cette zone que le reste des provinces. ',
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Icon(Icons.share,
+                      color: Colors.lightGreen,),
+                    Icon(Icons.forum,
+                      color: Colors.lightGreen,)
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.lightGreen,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: ClipRRect(
+                  borderRadius: new BorderRadius.circular(15.0),
+                  child: Image.asset('assets/img/oignon.jpg'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 1.0, left: 5, right: 5, bottom: 8),
+                    child: Text(
+                      'Cette campagne a étét menée dans le but de connaitre la quantité de citron produite dans la zone de Makokou. Il ressort de la que les citrons sont beaucoup plus produit dans cette zone que le reste des provinces. ',
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Icon(Icons.share,
+                      color: Colors.lightGreen,),
+                    Icon(Icons.forum,
+                      color: Colors.lightGreen,)
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.lightGreen,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: ClipRRect(
+                  borderRadius: new BorderRadius.circular(15.0),
+                  child: Image.asset('assets/img/tomates.jpg'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 1.0, left: 5, right: 5, bottom: 8),
+                    child: Text(
+                      'Cette campagne a étét menée dans le but de connaitre la quantité de citron produite dans la zone de Makokou. Il ressort de la que les citrons sont beaucoup plus produit dans cette zone que le reste des provinces. ',
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Icon(Icons.share,
+                      color: Colors.lightGreen,),
+                    Icon(Icons.forum,
+                      color: Colors.lightGreen,)
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.lightGreen,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: ClipRRect(
+                  borderRadius: new BorderRadius.circular(15.0),
+                  child: Image.asset('assets/img/citrouille.jpg'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 1.0, left: 5, right: 5, bottom: 8),
+                    child: Text(
+                      'Cette campagne a étét menée dans le but de connaitre la quantité de citron produite dans la zone de Makokou. Il ressort de la que les citrons sont beaucoup plus produit dans cette zone que le reste des provinces. ',
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Icon(Icons.share,
+                      color: Colors.lightGreen,),
+                    Icon(Icons.forum,
+                      color: Colors.lightGreen,)
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.lightGreen,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
